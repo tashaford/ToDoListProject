d03567d63cb96a53a10ee1ce176ef5d88875741d
Natasha Ford <neford3991@gmail.com>
2017-07-14 11:06:24 +0100

    Final all.

--- app/src/test/java/com/example/natashaford/todolist/TestTask.java
+++ app/src/test/java/com/example/natashaford/todolist/TestTask.java
@@ -11,38 +11,77 @@ import static org.junit.Assert.assertEquals;
 
 public class TestTask {
 
-
     Task task;
 
     @Before
     public void before() {
-        task = new Task("Create object diagram","Write down notes firstly. Once finished make a professional looking one for notes to submit");
+        task = new Task(2, "Create object diagram","Write down notes firstly. Once finished make a professional looking one for notes to submit", false, "work", false);
+    }
+
+    @Test
+    public void getIdTest() {
+        assertEquals(2, task.getId());
+    }
+
+    @Test
+    public void setIdTest() {
+        task.setId(1);
+        assertEquals(1, task.getId());
     }
 
     @Test
     public void getTitleTest() {
         assertEquals("Create object diagram", task.getTitle());
-
     }
 
     @Test
     public void setTitleTest() {
         task.setTitle("Hello");
         assertEquals("Hello", task.getTitle());
-
     }
 
     @Test
     public void getDetailsTest() {
         assertEquals("Write down notes firstly. Once finished make a professional looking one for notes to submit", task.getDetails());
-
     }
 
     @Test
     public void setDetailsTest() {
         task.setDetails("This is for practise.");
         assertEquals("This is for practise.", task.getDetails());
+    }
 
+    @Test
+    public void getCompletedTest() {
+        assertEquals(false, task.getCompleted());
+    }
+
+    @Test
+    public void setCompletedTest() {
+        task.setCompleted(true);
+        assertEquals(true, task.getCompleted());
+    }
+
+    @Test
+    public void getCategoryTest() {
+        assertEquals("work", task.getCategory());
+    }
+
+    @Test
+    public void setCCategoryTest() {
+        task.setCategory("Home");
+        assertEquals("Home", task.getCategory());
+    }
+
+    @Test
+    public void getPriorityTest() {
+        assertEquals(false, task.getPriority());
+    }
+
+    @Test
+    public void setPriorityTest() {
+        task.setPriority(true);
+        assertEquals(true, task.getPriority());
     }
 
 }
